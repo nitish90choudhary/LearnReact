c071c8d91137d72097c831047687b59067a708c3
nitish.choudhary <nitish.choudhary@servicenow.com>
2019-05-05 22:12:53 +0200

    Setting up Router configurations

---
+++ src/components/Home.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Header from './Header';
+import RecipeList from './RecipeList';
+import RecipeDetail from './RecipeDetails';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [],
+      currentRecipe: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${API_URL}/v1/recipes`)
+      .then(res => res.json())
+      .then((recipes) => {
+        this.setState({ recipes });
+      });
+  }
+
+    onRecipeClick = (id) => {
+      fetch(`${API_URL}/v1/recipes/${id}`)
+        .then(res => res.json())
+        .then((recipe) => {
+          this.setState({ currentRecipe: recipe });
+        });
+    }
+
+    render() {
+      const { recipes, currentRecipe } = this.state;
+      return (
+        <div>
+          <main className="px4 flex">
+            <RecipeList
+              recipes={recipes}
+              style={{ flex: 3 }}
+              onClick={this.onRecipeClick}
+            />
+            <RecipeDetail
+              className="ml4"
+              recipe={currentRecipe}
+              style={{ flex: 5 }}
+            />
+          </main>
+        </div>
+      );
+    }
+}
+
+export default Home;

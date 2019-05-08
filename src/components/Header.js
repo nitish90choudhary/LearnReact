1b85266501e5a0cd71bf09a061aa4f64858bc0b7
nitish.choudhary <nitish.choudhary@servicenow.com>
2019-05-08 15:05:01 +0200

    Added NavLink to the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const HeaderLink = ({ children, ...props }) => (
+  <NavLink
+    exact
+    to="/"
+    className="p1 mx4 black text-decoration-none"
+    activeClassName="bg-blue"
+    {...props}
+  >
+    {children}
+  </NavLink>
+);
 
 const Header = () => (
-  <header className="px4">
+  <header className="flex items-center justify-between  px4">
     <h1 className="h1">🍽My Recipe</h1>
+    <nav>
+      <HeaderLink to="/">Home</HeaderLink>
+      <HeaderLink to="/favorites">Favorites</HeaderLink>
+    </nav>
   </header>
 );
+
+HeaderLink.propTypes = {
+  children: PropTypes.node,
+};
 export default Header;
